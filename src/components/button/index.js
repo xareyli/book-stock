aca9f4bc9ae5b3801abf6627e74e08b366d7e240
xareyli <xareyli@gmail.com>
2022-10-11 18:28:37 +0300

    Added promo block to the main page

---
+++ src/components/button/index.js
@@ -0,0 +1,5 @@
+import style from './style.scss';
+
+const Button = ({ children }) => <button class={style.button}>{children}</button>;
+
+export default Button;
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,19 +1,11 @@
-import { useDispatch, useSelector } from 'react-redux';
 import style from './style.css';
 
-import { authSlice } from '../../redux/reducers/AuthSlice';
+import Promo from '../../components/main-page/promo';
 
-const Home = () => {
-    const state = useSelector(state => state.authReducer);
-    const { increment } = authSlice.actions;
-    const dispatch = useDispatch();
-
-    return (
-        <div class={style.home}>
-            <h1>{state.isAuthenticated}</h1>
-            <p onClick={() => dispatch(increment(1))}>This is the Home component.</p>
-        </div>
-    );
-};
+const Home = () => (
+    <div class={style.home}>
+        <Promo />
+    </div>
+);
 
 export default Home;
