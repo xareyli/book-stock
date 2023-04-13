23f9cd6c8f9ee93b3da32a54076eeafdafcd686a
xareyli <xareyli@gmail.com>
2023-04-13 14:56:09 +0300

    refactor: move promo to a separate component

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -9,36 +9,11 @@ import Catalog from '../../components/main-page/catalog';
 import SectionTemplate from '../../components/main-page/section-template';
 import Stock from '../../components/main-page/stock';
 import BlockTitle from '../../components/ui/block-title';
-import Button from '../../components/ui/button';
+import Promo from '../../components/main-page/promo';
 
 const Home = () => (
     <main>
-        <section class={style.promo}>
-            <div class={`${style.promo__blackout} _blackout _blackout--visible`} />
-            <div class={`${style.promo__wrapper} _container`}>
-                <h2 class={style.promo__title}>
-                    <em>Ваши любимые книги</em>
-                    <br />
-                    НА КАЖДЫЙ ДЕНЬ
-                </h2>
-
-                <div class={`${style.promo__quote} ${style.quote}`}>
-                    <p class={style.quote__text}>
-                        Человек, который читает, проживает тысячу
-                        <br />
-                        жизней. Тот, кто не читает, проживает лишь
-                        <br />
-                        одну.
-                    </p>
-
-                    <div class={style.quote__author}>
-                        <span>Дж.Мартин</span>
-                    </div>
-                </div>
-
-                <Button>Каталог</Button>
-            </div>
-        </section>
+        <Promo />
 
         <Catalog className={style.homeSection} />
 
