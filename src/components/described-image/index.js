884271d1f8d75a79e30ed4bbfb8ae1ece9cb5fc9
xareyli <xareyli@gmail.com>
2023-04-13 14:37:08 +0300

    chore: add link to cafeteria

--- src/components/described-image/index.js
+++ src/components/described-image/index.js
@@ -1,16 +1,19 @@
 import style from './style.scss';
 
 import IBG from '../ibg';
+import { Link } from 'preact-router';
 
-const DescribedImage = ({ img, text, textColor = 'black', linkText }) => (
+const DescribedImage = ({ img, text, textColor = 'black', href, linkText }) => (
     <div class={style.container}>
         <div class={style.block}>
             <p class={`${style.text} ${style[`text--${textColor}`]}`}>{text}</p>
 
             {linkText ? (
-                <a href="#" class={style.link}>
-                    {linkText}
-                </a>
+                <Link activeClassName="active" href={href}>
+                    <span class={style.link}>
+                        {linkText}
+                    </span>
+                </Link>
             ) : (
                 ''
             )}
